0b1b27b588026e67912905d4c90954cab780c6e5
Charlie W <cw5790@gmail.com>
2024-02-25 18:31:50 +0000

    Update button stuff

--- button/src/button.tsx
+++ button/src/button.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import styles from './button.module.scss';
 import { type VariantProps, cva } from 'cva';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
+
+import styles from './button.module.scss';
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
